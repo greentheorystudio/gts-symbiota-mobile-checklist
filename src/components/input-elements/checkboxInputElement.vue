4bbac39ac1023233d83ee9b4a8bacde7e7f84420
Ben Brandt <benjamin.brandt@nau.edu>
2025-12-27 08:56:39 -0700

    WIP

---
+++ src/components/input-elements/checkboxInputElement.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="row inline q-gutter-x-xs">
+        <div>
+            <q-checkbox v-model="checkboxValue" :label="label" @update:model-value="processValueChange" :tabindex="tabindex" dense></q-checkbox>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted, defineEmits, ref, toRefs, watch } from 'vue';
+
+const props = defineProps({
+    label: {
+        type: String,
+        default: ''
+    },
+    tabindex: {
+        type: Number,
+        default: 0
+    },
+    value: {
+        type: String,
+        default: null
+    }
+});
+
+const emit = defineEmits(['update:value']);
+
+const checkboxValue = ref(false);
+const propsRefs = toRefs(props);
+
+watch(propsRefs.value, () => {
+    setCheckboxValue();
+});
+
+function processValueChange(val) {
+    setCheckboxValue();
+    emit('update:value', (val ? 1 : null));
+}
+
+function setCheckboxValue() {
+    checkboxValue.value = Number(props.value) === 1;
+}
+
+onMounted(() => {
+    setCheckboxValue();
+});
+</script>
