977e511b27708dd95e16ac89a7869e38e3fceae0
Ben Brandt <benjamin.brandt@nau.edu>
2026-01-04 11:20:03 -0700

    WIP

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,23 +3,41 @@
         <q-header elevated>
             <q-toolbar>
                 <q-toolbar-title>
-                    <div class="cursor-pointer" @click="$router.push('/');">
+                    <div class="cursor-pointer">
                         Symbiota Mobile Checklist
                     </div>
                 </q-toolbar-title>
-
                 <div class="row q-gutter-sm">
                     <q-btn flat dense round icon="download_for_offline" aria-label="Download" @click="showDownloadPopup = true" />
                     <q-btn flat dense round icon="settings" aria-label="Management" @click="showManagementPopup = true" />
                     <q-btn flat dense round icon="info" aria-label="Help" @click="showInformationPopup = true" />
                 </div>
             </q-toolbar>
+            <template v-if="checklistArr.length > 0">
+                <div class="full-width q-px-sm">
+                    <selectorInputElement label="Select Checklist" :options="checklistArr" option-label="name" option-value="clid" :value="Number(checklistId) > 0 ? checklistId.toString() : ''" @update:value="processChecklistSelection"></selectorInputElement>
+                </div>
+                <div v-if="showTopOptions" class="full-width q-px-sm q-pt-sm">
+                    <q-card>
+                        <q-card-section class="bg-white" style="height: 200px;">
+
+                        </q-card-section>
+                    </q-card>
+                </div>
+                <div role="button" class="q-pa-none q-ma-none full-width row justify-center" :class="!showTopOptions ? '' : 'hidden'" @click="showTopOptions = true">
+                    <q-icon color="white" size="lg" name="arrow_drop_down"></q-icon>
+                </div>
+                <div role="button" class="q-pa-none q-ma-none full-width row justify-center" :class="showTopOptions ? '' : 'hidden'" @click="showTopOptions = false">
+                    <q-icon color="white" size="lg" name="arrow_drop_up"></q-icon>
+                </div>
+            </template>
         </q-header>
-
-        <q-drawer v-model="leftDrawerOpen" bordered>
-
+        <q-drawer v-model="leftDrawerOpen" overlay bordered elevated>
+            <div>Panel</div>
+            <div class="q-mini-drawer-hide absolute" style="top: calc(50% - 13px); right: 0">
+                <q-btn square padding="25px 0" color="primary" icon="arrow_left" @click="toggleLeftDrawer"></q-btn>
+            </div>
         </q-drawer>
-
         <q-page-container>
             <router-view />
             <appInformationPopup
@@ -41,11 +59,13 @@
         </q-page-container>
     </q-layout>
 </template>
-
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, provide, ref } from 'vue';
+import 'animate.css';
 
 import { useChecklistStore } from 'src/stores/checklist';
+
+import selectorInputElement from 'src/components/input-elements/selectorInputElement.vue';
 
 import appInformationPopup from 'src/components/popups/appInformationPopup.vue';
 import appManagementPopup from 'src/components/popups/appManagementPopup.vue';
@@ -55,13 +75,21 @@ import taxonProfilePopup from 'src/components/popups/taxonProfilePopup.vue';
 const checklistStore = useChecklistStore();
 
 const checklistArr = computed(() => checklistStore.getChecklistArr);
+const checklistId = computed(() => checklistStore.getChecklistId);
 const leftDrawerOpen = ref(false);
 const showDownloadPopup = ref(false);
 const showInformationPopup = ref(false);
 const showManagementPopup = ref(false);
 const showTaxonProfilePopup = ref(false);
+const showTopOptions = ref(false);
+
+function processChecklistSelection(clid: number) {
+    checklistStore.setCurrentChecklist(clid);
+}
 
 function toggleLeftDrawer() {
     leftDrawerOpen.value = !leftDrawerOpen.value;
 }
+
+provide('toggleLeftDrawer', toggleLeftDrawer);
 </script>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,21 +1,34 @@
 <template>
-    <q-page class="flex flex-center">
-        <div class="text-h6 text-bold">
-            <template v-if="checklistArr.length > 0">
-                <div>Select a checklist from the dropdown above to view</div>
-            </template>
-            <template v-else-if="remoteChecklistArr.length > 0">
-                <div>Tap the Download button in the top right corner to download checklists to your device</div>
-            </template>
-            <template v-else-if="!remoteConnectionEstablished">
-                <div>Out of internet connection</div>
-            </template>
-        </div>
+    <q-page>
+        <template v-if="Number(checklistId) > 0">
+            <div class="fit">
+                <q-page-sticky position="left" :offset="[-5, 0]">
+                    <q-btn square padding="25px 0" color="primary" icon="arrow_right" @click="toggleLeftDrawer"></q-btn>
+                </q-page-sticky>
+                <div>
+
+                </div>
+            </div>
+        </template>
+        <template v-else>
+            <div class="fit flex flex-center">
+                <div class="text-h6 text-bold">
+                    <template v-if="checklistArr.length > 0">
+                        <div>Select a checklist from the dropdown above to view</div>
+                    </template>
+                    <template v-else-if="remoteChecklistArr.length > 0">
+                        <div>Tap the Download button in the top right corner to download checklists to your device</div>
+                    </template>
+                    <template v-else-if="!remoteConnectionEstablished">
+                        <div>Out of internet connection</div>
+                    </template>
+                </div>
+            </div>
+        </template>
     </q-page>
 </template>
-
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, inject } from 'vue';
 
 import { useChecklistStore } from 'src/stores/checklist';
 import { useChecklistRemoteStore } from 'src/stores/checklist-remote';
@@ -24,6 +37,9 @@ const checklistStore = useChecklistStore();
 const checklistRemoteStore = useChecklistRemoteStore();
 
 const checklistArr = computed(() => checklistStore.getChecklistArr);
+const checklistId = computed(() => checklistStore.getChecklistId);
 const remoteChecklistArr = computed(() => checklistRemoteStore.getChecklistArr);
 const remoteConnectionEstablished = computed(() => checklistRemoteStore.getRemoteConnectionEstablished);
+
+const toggleLeftDrawer: any = inject('toggleLeftDrawer');
 </script>
