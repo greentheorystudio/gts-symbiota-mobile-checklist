8b0506736eb1a3251a7b273ace5d069749634e65
Ben Brandt <benjamin.brandt@nau.edu>
2026-01-22 19:11:43 -0700

    WIP

--- src/components/popups/taxonProfilePopup.vue
+++ src/components/popups/taxonProfilePopup.vue
@@ -13,35 +13,32 @@
             </q-card-section>
             <q-card-section class="q-pl-md q-py-none">
                 <q-scroll-area :style="scrollerStyle">
-                    <div v-if="taxon['family']" class="text-subtitle1"><span class="text-bold">Family: </span>{{ taxon['family'] }}</div>
-                    <div v-if="taxon['vernacularJson']" class="text-subtitle1">
-                        {{ getVernacularStrFromArr(taxon['vernacularJson']) }}
-                    </div>
-                    <div v-if="taxon['synonymyJson']" class="text-subtitle1 text-italic">
-                        {{ getSynonymStrFromArr(taxon['synonymyJson']) }}
-                    </div>
-                    <div v-if="taxon['habitat']" class="text-subtitle1"><span class="text-bold">Habitat: </span>{{ taxon['habitat'] }}</div>
-                    <div v-if="taxon['abundance']" class="text-subtitle1"><span class="text-bold">Abundance: </span>{{ taxon['abundance'] }}</div>
-                    <div v-if="taxon['notes']" class="text-subtitle1"><span class="text-bold">Notes: </span>{{ taxon['notes'] }}</div>
-                    <div v-if="taxon['descriptionJson'] && taxon['descriptionJson']['statements'].length > 0" class="column">
-                        <div class="q-mb-sm text-subtitle1 text-bold">
-                            {{ taxon['descriptionJson']['caption'] + ':' }}
+                    <div class="q-pa-sm column q-gutter-xs">
+                        <div v-if="taxon['family']" class="text-subtitle1"><span class="text-bold">Family: </span>{{ taxon['family'] }}</div>
+                        <div v-if="taxon['vernacularJson']" class="text-subtitle1">
+                            {{ getVernacularStrFromArr(taxon['vernacularJson']) }}
                         </div>
-                        <div class="q-pl-md column q-col-gutter-xs">
-                            <template v-for="statement in taxon['descriptionJson']['statements']">
+                        <div v-if="taxon['synonymyJson']" class="text-subtitle1 text-italic">
+                            {{ getSynonymStrFromArr(taxon['synonymyJson']) }}
+                        </div>
+                        <div v-if="taxon['habitat']" class="text-subtitle1"><span class="text-bold">Habitat: </span>{{ taxon['habitat'] }}</div>
+                        <div v-if="taxon['abundance']" class="text-subtitle1"><span class="text-bold">Abundance: </span>{{ taxon['abundance'] }}</div>
+                        <div v-if="taxon['notes']" class="text-subtitle1"><span class="text-bold">Notes: </span>{{ taxon['notes'] }}</div>
+                        <template v-if="descriptionData && descriptionData['statements'].length > 0">
+                            <template v-for="statement in descriptionData['statements']">
                                 <div class="text-body1">
-                                    <span v-if="statement['heading'] && Number(statement['displayheader']) === 1">{{ statement['heading'] + ': ' }}</span>
+                                    <span v-if="statement['heading'] && Number(statement['displayheader']) === 1" class="text-bold">{{ statement['heading'] + ': ' }}</span>
                                     <span v-html="statement['statement']"></span>
                                 </div>
                             </template>
-                        </div>
-                    </div>
-                    <div v-if="taxonImageArr.length > 0" class="q-mt-sm column q-col-gutter-sm">
-                        <template v-for="image in taxonImageArr">
-                            <div>
-                                <q-img class="rounded-borders" :width="imageWidth" :src="image['contentData']" fit="contain" :alt="(image['alttext'] ? image['alttext'] : taxon['sciname'])"></q-img>
-                            </div>
                         </template>
+                        <div v-if="taxonImageArr.length > 0" class="q-mt-sm column q-col-gutter-sm">
+                            <template v-for="image in taxonImageArr">
+                                <div>
+                                    <q-img class="rounded-borders" :width="imageWidth" :src="image['contentData']" fit="contain" :alt="(image['alttext'] ? image['alttext'] : taxon['sciname'])"></q-img>
+                                </div>
+                            </template>
+                        </div>
                     </div>
                 </q-scroll-area>
             </q-card-section>
@@ -82,15 +79,22 @@ const checklistImageData = computed(() => {
         return checklistStore.getImageContentData;
     }
 });
+const descriptionData = computed(() => {
+    let returnData = null;
+    if(props.taxon['descriptionJson']){
+        returnData = JSON.parse(props.taxon['descriptionJson']);
+    }
+    return returnData;
+});
 const displayPopup = ref(false);
 const headerHeight = ref(0);
 const imageWidth = computed(() => {
-    return (cardWidth.value - 30) + 'px';
+    return (cardWidth.value - 40) + 'px';
 });
 const mapImageContentData = ref(null);
 const propsRefs = toRefs(props);
 const scrollerStyle = computed(() => {
-    return 'width: ' + cardWidth.value + 'px;height: ' + (cardHeight.value - headerHeight.value) + 'px;';
+    return 'width: ' + (cardWidth.value - 10) + 'px;height: ' + (cardHeight.value - headerHeight.value) + 'px;';
 });
 const taxonImageArr = computed(() => {
     return (checklistImageData['value'].hasOwnProperty(props.taxon['tid']) && checklistImageData['value'][props.taxon['tid']].length > 0) ? checklistImageData['value'][props.taxon['tid']] : [];
